3c0acccf42375802617946b3a1ef39c12180db80
shuWshu <tamushun0728@gmail.com>
2023-08-13 00:27:03 +0900

    flaskのテストコード追加

--- flask/testapp/views.py
+++ flask/testapp/views.py
@@ -1,9 +1,13 @@
+from flask import render_template
 from testapp import app
 
-@app.route('/') # url
-def index():
-    return 'Hellow World!'
 
-@app.route('/test')
-def other1():
-    return "テストページです！"
+@app.route('/')
+def index():
+    # htmlへデータを直接渡す
+    my_dict = {
+        'insert_something1': 'views.pyのinsert_something1部分です。',
+        'insert_something2': 'views.pyのinsert_something2部分です。',
+    }
+    return render_template('testapp/index.html', my_dict=my_dict)
+
